b1af907021cb989189e4e5bd2d1b95220bd6b2c6
kadmila <kadmila.abyss@gmail.com>
2026-02-13 05:01:43 +0900

    scenario runner

--- mininet-control/abyss_test/main.go
+++ mininet-control/abyss_test/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"crypto/ed25519"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path"
 
 	"github.com/kadmila/Abyss-Browser/abyss_core/ahost"
 	"golang.org/x/crypto/ssh"
@@ -15,9 +17,29 @@ func main() {
 	// Parse CLI arguments
 	var n_peer int
 	var id string
+	var contact_dir string
+	var scenario_path string
+	var output_path string
 	flag.IntVar(&n_peer, "n_peer", 0, "number of peers")
 	flag.StringVar(&id, "id", "", "host id")
+	flag.StringVar(&contact_dir, "contact_dir", "", "path to directory for sharing contact information")
+	flag.StringVar(&scenario_path, "scenario", "", "path to scenario JSON file")
+	flag.StringVar(&output_path, "out", "", "path to output file")
 	flag.Parse()
+
+	// Parse scenario file if provided
+	var scenario []map[string]string
+	if scenario_path != "" {
+		scenarioData, err := os.ReadFile(scenario_path)
+		if err != nil {
+			log.Fatalf("Error reading scenario file: %v", err)
+		}
+		err = json.Unmarshal(scenarioData, &scenario)
+		if err != nil {
+			log.Fatalf("Error parsing scenario JSON: %v", err)
+		}
+		log.Printf("Loaded scenario with %d entries", len(scenario))
+	}
 
 	// Read ../credentials/{id}.pem and parse key
 	key_pem, err := os.ReadFile(fmt.Sprintf("../credentials/%s.pem", id))
@@ -47,5 +69,32 @@ func main() {
 
 	go host.Serve()
 
-	fmt.Println(host.ID(), host.LocalAddrCandidates())
+	// Write contact information
+	rc_f, err := os.OpenFile(path.Join(contact_dir, id+"_rc"), os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := rc_f.WriteString(host.RootCertificate()); err != nil {
+		log.Fatal(err)
+	}
+	rc_f.Close() // Ensure the file is closed
+	hs_f, err := os.OpenFile(path.Join(contact_dir, id+"_hs"), os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := hs_f.WriteString(host.HandshakeKeyCertificate()); err != nil {
+		log.Fatal(err)
+	}
+	hs_f.Close() // Ensure the file is closed
+	id_f, err := os.OpenFile(path.Join(contact_dir, id+"_id"), os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := id_f.WriteString(host.ID()); err != nil {
+		log.Fatal(err)
+	}
+	id_f.Close() // Ensure the file is closed
+
+	scenario_runner := NewScenarioRunner(contact_dir, scenario, host, output_path)
+	scenario_runner.Run()
 }
---
+++ mininet-control/abyss_test/scenario_runner.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/kadmila/Abyss-Browser/abyss_core/ahost"
+	"github.com/kadmila/Abyss-Browser/abyss_core/and"
+)
+
+// ScenarioRunner executes a sequence of actions defined in a scenario
+type ScenarioRunner struct {
+	contact_dir string
+	scenario    []map[string]string
+	host        *ahost.AbyssHost
+	out_f       *os.File
+
+	world_mtx sync.Mutex
+	world     *and.World
+}
+
+// NewScenarioRunner creates a new ScenarioRunner with the given scenario and host
+func NewScenarioRunner(contact_dir string, scenario []map[string]string, host *ahost.AbyssHost, output_path string) *ScenarioRunner {
+	out_f, err := os.Create(output_path)
+	if err != nil {
+		log.Fatalf("Error reading scenario file: %v", err)
+	}
+	return &ScenarioRunner{
+		contact_dir: contact_dir,
+		scenario:    scenario,
+		host:        host,
+		out_f:       out_f,
+	}
+}
+
+// Run executes the scenario by iterating over each step and waiting until the specified timestamp
+func (sr *ScenarioRunner) Run() error {
+	go sr.HandleEvents()
+
+	for i, step := range sr.scenario {
+		timeStr, ok := step["time"]
+		if !ok {
+			log.Printf("Warning: Step %d missing 'time' field, skipping", i)
+			continue
+		}
+
+		timestamp, err := strconv.ParseInt(timeStr, 10, 64)
+		if err != nil {
+			log.Printf("Error: Step %d has invalid timestamp '%s': %v", i, timeStr, err)
+			continue
+		}
+
+		targetTime := time.Unix(timestamp, 0)
+		now := time.Now()
+
+		if targetTime.After(now) {
+			waitDuration := targetTime.Sub(now)
+			time.Sleep(waitDuration)
+		}
+
+		// Action
+		switch step["do"] {
+		case "add":
+
+			peer_id := step["id"]
+			rc, err := os.ReadFile(path.Join(sr.contact_dir, peer_id+"_rc"))
+			if err != nil {
+				log.Fatalf("unable to read file: %v", err)
+			}
+			hs, err := os.ReadFile(path.Join(sr.contact_dir, peer_id+"_hs"))
+			if err != nil {
+				log.Fatalf("unable to read file: %v", err)
+			}
+			sr.host.AppendKnownPeer(string(rc), string(hs))
+
+		case "dial":
+
+			peer_id := step["id"]
+			id_hash, err := os.ReadFile(path.Join(sr.contact_dir, peer_id+"_id"))
+			if err != nil {
+				log.Fatalf("unable to read file: %v", err)
+			}
+			sr.host.Dial(string(id_hash))
+
+		case "join":
+
+			peer_id := step["id"]
+			id_hash, err := os.ReadFile(path.Join(sr.contact_dir, peer_id+"_id"))
+			if err != nil {
+				log.Fatalf("unable to read file: %v", err)
+			}
+
+			sr.world_mtx.Lock()
+			if sr.world != nil {
+				sr.host.CloseWorld(sr.world) // This automatically frees world path
+				fmt.Fprintf(sr.out_f, "%d X %v\n", time.Now().UnixMilli(), sr.world.SessionID())
+			}
+			sr.world = nil
+			sr.world_mtx.Unlock()
+
+			for i := range 100 {
+				if i == 99 {
+					log.Println("Error: Failed to join. This is a failure.")
+					break
+				}
+
+				sr.world_mtx.Lock()
+				sr.world, err = sr.host.JoinWorld(string(id_hash), "/")
+				sr.world_mtx.Unlock()
+
+				if err == nil {
+					break
+				}
+				time.Sleep(time.Millisecond * 100)
+			}
+
+		case "open":
+
+			sr.world_mtx.Lock()
+			if sr.world != nil {
+				sr.host.CloseWorld(sr.world) // This automatically frees world path
+				fmt.Fprintf(sr.out_f, "%d X %v\n", time.Now().UnixMilli(), sr.world.SessionID())
+			}
+			sr.world = sr.host.OpenWorld("https://www.example.com")
+			sr.world_mtx.Unlock()
+
+		}
+	}
+	return nil
+}
+
+func (sr *ScenarioRunner) HandleEvents() {
+	event_ch := sr.host.GetEventCh()
+
+	for {
+		any_event, ok := <-event_ch
+		if !ok {
+			break
+		}
+
+		sr.world_mtx.Lock()
+
+		switch event := any_event.(type) {
+		case *and.EANDWorldEnter:
+
+			if sr.world != nil && sr.world.SessionID() == event.World.SessionID() {
+				sr.host.ExposeWorldForJoin(sr.world, "/") // this should not fail.
+				fmt.Fprintf(sr.out_f, "%d E %v\n", time.Now().UnixMilli(), event.World.SessionID())
+			}
+
+		case *and.EANDSessionRequest:
+
+			if sr.world != nil && sr.world.SessionID() == event.World.SessionID() {
+				sr.host.AcceptWorldSession(sr.world, event.Peer.ID(), event.SessionID)
+			}
+
+		case *and.EANDSessionReady:
+
+			if sr.world != nil && sr.world.SessionID() == event.World.SessionID() {
+				fmt.Fprintf(sr.out_f, "%d J %v\n", time.Now().UnixMilli(), event.SessionID)
+			}
+
+		case *and.EANDSessionClose:
+
+			if sr.world != nil && sr.world.SessionID() == event.World.SessionID() {
+				fmt.Fprintf(sr.out_f, "%d L %v\n", time.Now().UnixMilli(), event.SessionID)
+			}
+
+		case *and.EANDObjectAppend:
+		case *and.EANDObjectDelete:
+		case *and.EANDWorldLeave: // join failure.
+
+			if sr.world != nil && sr.world.SessionID() == event.World.SessionID() {
+				sr.world = nil
+				fmt.Fprintf(sr.out_f, "%d X %v\n", time.Now().UnixMilli(), event.World.SessionID())
+			}
+		}
+
+		sr.world_mtx.Unlock()
+	}
+}
